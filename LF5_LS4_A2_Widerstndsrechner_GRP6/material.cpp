6d9f07619c6e79994ae06bfc42315ca015596944
Alexander Königs <alexander.koenigs@outlook.de>
2021-05-21 16:10:54 +0200

    Array korrigiert

--- LF5_LS4_A2_Widerstndsrechner_GRP6/material.cpp
+++ LF5_LS4_A2_Widerstndsrechner_GRP6/material.cpp
@@ -20,7 +20,7 @@ using namespace std;
 double SpecificResistanceOfMaterial[6] = { 0.0165, 0.0178, 0.023, 0.0286, 0.055, 0.12 };
 double ElectricalConductivityOfMaterial[6] = { 60.6, 56.2, 43.5, 35.0, 18.2, 8.3 };
 double TemperatureCoefficientA[6] = { 0.0038, 0.0039, 0.0039, 0.0037, 0.0048, 0.0042 };
-double TemperatureCoefficientB[6] = { 0.7, 0.6, 0.5, 1.3, 18.2, 8.3 };
+double TemperatureCoefficientB[6] = { 0.7, 0.6, 0.5, 1.3, 1.0, 6.0 };
 
 double MaterialSpez(int Konstante, int Material)
 {
